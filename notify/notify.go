5088a077865a2ec50e7e730bda9cdf7671d7a077
Kevin C. Krinke <kevin@krinke.ca>
2023-08-30 11:56:55 -0400

    notify: bugfix for incorrectly sprintf-formatting non-format strings

--- notify/notify.go
+++ notify/notify.go
@@ -33,18 +33,30 @@ func (n *Notifier) Level() (level Level) {
 
 func (n *Notifier) Debug(format string, argv ...interface{}) {
 	if n.level > Info {
+		if len(argv) == 0 {
+			argv = append(argv, format)
+			format = "%s"
+		}
 		fmt.Printf(format, argv...)
 	}
 }
 
 func (n *Notifier) Info(format string, argv ...interface{}) {
 	if n.level > Quiet {
+		if len(argv) == 0 {
+			argv = append(argv, format)
+			format = "%s"
+		}
 		fmt.Printf(format, argv...)
 	}
 }
 
 func (n *Notifier) Error(format string, argv ...interface{}) {
 	if n.level > Quiet {
+		if len(argv) == 0 {
+			argv = append(argv, format)
+			format = "%s"
+		}
 		fmt.Fprintf(os.Stderr, format, argv...)
 	}
 }
