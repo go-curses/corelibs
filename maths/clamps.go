081a579a61fd1a31c3253369ffdc6be5aa59d07c
Kevin C. Krinke <kevin@krinke.ca>
2024-01-02 12:08:23 -0500

    maths: clamps docstrings

--- maths/clamps.go
+++ maths/clamps.go
@@ -18,6 +18,10 @@ import (
 	"math"
 )
 
+// Clamp returns the value if it is greater-than-or-equal-to the minimum and
+// less-than-or-equal-to the maximum arguments; if the value is less than the
+// minimum, the minimum is returned; if the value is greater than the maximum,
+// the maximum is returned
 func Clamp[V Number](value, min, max V) V {
 	if value >= min && value <= max {
 		return value
@@ -28,6 +32,8 @@ func Clamp[V Number](value, min, max V) V {
 	return min
 }
 
+// Floor returns the value if it is greater-than-or-equal-to the minimum and
+// returns the minimum otherwise
 func Floor[V Number](value, min V) V {
 	if value < min {
 		return min
@@ -35,6 +41,8 @@ func Floor[V Number](value, min V) V {
 	return value
 }
 
+// Ceil returns the value if it is less-than-or-equal-to the maximum and
+// returns the maximum otherwise
 func Ceil[V Number](value, max V) V {
 	if value > max {
 		return max
@@ -42,14 +50,17 @@ func Ceil[V Number](value, max V) V {
 	return value
 }
 
+// Round returns the value rounded to the nearest whole value
 func Round[V Decimal](x V) (rounded int) {
 	return int(math.Floor(float64(x) + 0.5))
 }
 
+// RoundUp returns the value rounded up
 func RoundUp[V Decimal](value V) (rounded int) {
 	return int(math.Ceil(float64(value)))
 }
 
+// RoundDown returns the value rounded down
 func RoundDown[V Decimal](value V) (rounded int) {
 	return int(math.Floor(float64(value)))
 }
