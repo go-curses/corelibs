00b298fa7e69a2d13fb4180c5c10e414d684711b
Kevin C. Krinke <kevin@krinke.ca>
2024-01-02 14:58:58 -0500

    notify: rewrite for buildable construction; added docstrings; added read/write mutex locking; added methods for accessing and modifying the underlying STDOUT/STDERR io.Writer instances at runtime

--- notify/notify.go
+++ notify/notify.go
@@ -18,42 +18,140 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
+// Level specifies the verbosity of a Notifier
 type Level = int
 
 const (
+	// Quiet configures the Notifier to produce no output
 	Quiet Level = iota
+	// Error configures the Notifier to only produce error output
+	Error
+	// Info configures the Notifier to produce normal and error output
 	Info
+	// Debug configures the Notifier to produce all outputs
 	Debug
 )
 
-type Notifier struct {
+// Builder is the interface for constructing new Notifier instances
+type Builder interface {
+	// SetLevel modifies the level setting
+	SetLevel(level Level) Builder
+	// SetOut overrides the default os.Stdout setting
+	SetOut(w io.Writer) Builder
+	// SetErr overrides the default os.Stderr setting
+	SetErr(w io.Writer) Builder
+	// Make produces the built Notifier instance
+	Make() (n Notifier)
+}
+
+type Notifier interface {
+	// Level returns the verbosity of this Notifier
+	Level() (level Level)
+	// Stdout returns the io.Writer associated with normal output
+	Stdout() (w io.Writer)
+	// Stderr returns the io.Writer associated with error output
+	Stderr() (w io.Writer)
+	// ModifyLevel modifies the configured level setting
+	ModifyLevel(level Level) Notifier
+	// ModifyOut modifies the configured output writer
+	ModifyOut(w io.Writer) Notifier
+	// ModifyErr modifies the configured error writer
+	ModifyErr(w io.Writer) Notifier
+	// Debug passes the arguments to fmt.Fprintf using normal output and only
+	// has effect when the Level is set to Debug
+	Debug(format string, argv ...interface{})
+	// Info passes the arguments to fmt.Fprintf using normal output and only
+	// has effect when the Level is set to Info or Debug
+	Info(format string, argv ...interface{})
+	// Error passes the arguments to fmt.Fprintf using normal output and only
+	// has effect when the Level is set to Info or Debug
+	Error(format string, argv ...interface{})
+}
+
+type CNotifier struct {
 	level  Level
 	stdout io.Writer
 	stderr io.Writer
+
+	sync.RWMutex
 }
 
-func New(level Level) *Notifier {
-	return &Notifier{
+// New constructs a new Builder instance
+func New(level Level) Builder {
+	return &CNotifier{
 		level:  level,
 		stdout: os.Stdout,
 		stderr: os.Stderr,
 	}
 }
 
-func (n *Notifier) Set(level Level, stdout io.Writer, stderr io.Writer) {
+func (n *CNotifier) SetLevel(level Level) Builder {
 	n.level = level
-	n.stdout = stdout
-	n.stderr = stderr
+	return n
 }
 
-func (n *Notifier) Level() (level Level) {
+func (n *CNotifier) SetOut(w io.Writer) Builder {
+	n.stdout = w
+	return n
+}
+
+func (n *CNotifier) SetErr(w io.Writer) Builder {
+	n.stderr = w
+	return n
+}
+
+func (n *CNotifier) Make() Notifier {
+	return n
+}
+
+func (n *CNotifier) ModifyLevel(level Level) Notifier {
+	n.Lock()
+	defer n.Unlock()
+	n.level = level
+	return n
+}
+
+func (n *CNotifier) ModifyOut(stdout io.Writer) Notifier {
+	n.Lock()
+	defer n.Unlock()
+	n.stdout = stdout
+	return n
+}
+
+func (n *CNotifier) ModifyErr(stderr io.Writer) Notifier {
+	n.Lock()
+	defer n.Unlock()
+	n.stderr = stderr
+	return n
+}
+
+func (n *CNotifier) Level() (level Level) {
+	n.RLock()
+	defer n.RUnlock()
 	return n.level
 }
 
-func (n *Notifier) Debug(format string, argv ...interface{}) {
-	if n.level > Info {
+func (n *CNotifier) Stdout() (w io.Writer) {
+	n.RLock()
+	defer n.RUnlock()
+	w = n.stdout
+	return
+}
+
+func (n *CNotifier) Stderr() (w io.Writer) {
+	n.RLock()
+	defer n.RUnlock()
+	w = n.stderr
+	return
+}
+
+func (n *CNotifier) Debug(format string, argv ...interface{}) {
+	n.Lock()
+	defer n.Unlock()
+	if n.stdout != nil && n.level > Info {
 		if len(argv) == 0 {
 			argv = append(argv, format)
 			format = "%s"
@@ -62,8 +160,10 @@ func (n *Notifier) Debug(format string, argv ...interface{}) {
 	}
 }
 
-func (n *Notifier) Info(format string, argv ...interface{}) {
-	if n.level > Quiet {
+func (n *CNotifier) Info(format string, argv ...interface{}) {
+	n.Lock()
+	defer n.Unlock()
+	if n.stdout != nil && n.level > Error {
 		if len(argv) == 0 {
 			argv = append(argv, format)
 			format = "%s"
@@ -72,8 +172,10 @@ func (n *Notifier) Info(format string, argv ...interface{}) {
 	}
 }
 
-func (n *Notifier) Error(format string, argv ...interface{}) {
-	if n.level > Quiet {
+func (n *CNotifier) Error(format string, argv ...interface{}) {
+	n.Lock()
+	defer n.Unlock()
+	if n.stderr != nil && n.level > Quiet {
 		if len(argv) == 0 {
 			argv = append(argv, format)
 			format = "%s"
